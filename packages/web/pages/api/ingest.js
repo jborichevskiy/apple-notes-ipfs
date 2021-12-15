6f75079aa2c03783e575bece2a6577e6594bfb87
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-12-15 17:08:15 -0600

    Truncate on hash symbol

--- packages/web/pages/api/ingest.js
+++ packages/web/pages/api/ingest.js
@@ -3,10 +3,12 @@ const { exec } = require("child_process");
 export default function handler(req, res) {
   console.log(req.body);
   console.log(req.data);
+  const id = req.body.url.split("#")[0];
   // TODO: implenent queueing/signaling while task in progress
   //   launch KeyboardMaestro script
+  console.log(`passing ${id} to KeyboardMaestro for ingestion`);
   exec(
-    `osascript -e \'tell application "Keyboard Maestro Engine" to do script "C40BD8A5-4BD1-4AED-AF90-9AF9C879388C" with parameter "${req.body.url}"\'`,
+    `osascript -e \'tell application "Keyboard Maestro Engine" to do script "C40BD8A5-4BD1-4AED-AF90-9AF9C879388C" with parameter "${id}"\'`,
     (error, stdout, stderr) => {
       if (error) {
         console.log(`error: ${error.message}`);
@@ -20,5 +22,5 @@ export default function handler(req, res) {
     }
   );
 
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ status: "ok" });
 }
