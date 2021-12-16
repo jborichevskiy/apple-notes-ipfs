a05f3ccd475b4055298e142dead0455033039759
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-12-16 11:25:11 -0600

    Fix script error

--- packages/web/pages/page.js
+++ packages/web/pages/page.js
@@ -48,6 +48,7 @@ export default function Page() {
     <div className="document">
       <script
         src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js"
+        async
         integrity="sha512-L03kznCrNOfVxOUovR6ESfCz9Gfny7gihUX/huVbQB9zjODtYpxaVtIaAkpetoiyV2eqWbvxMH9fiSv5enX7bw=="
         crossOrigin="anonymous"
         referrerPolicy="no-referrer"
