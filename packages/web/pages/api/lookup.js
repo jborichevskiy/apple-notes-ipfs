7dad39a901897a00fb51454d4f2eb5cc381f7881
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-12-15 18:24:51 -0600

    Add lookup endpoint

---
+++ packages/web/pages/api/lookup.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  const id = req.query.id;
+  const note = await prisma.note.findUnique({
+    where: {
+      appleId: id,
+    },
+  });
+  if (note) {
+    return res.status(200).json({ id: id, ipfsHash: note.ipfsHash });
+  } else {
+    return res.json({ error: "note not found" });
+  }
+}
