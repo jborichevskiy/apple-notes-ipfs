763f2cfe919e93c9701ee678d29d5b78c912f45d
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-12-15 23:25:26 -0600

    Add nicer index page

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -5,6 +5,15 @@ import { useState } from "react";
 
 export default function Home() {
   const [url, setUrl] = useState("");
+  const [noteData, setNoteData] = useState({});
+
+  async function handleCheckStatus() {
+    const id = url.split("/notes/")[1].split("#")[0];
+    const response = await fetch(`/api/lookup?id=${id}`);
+    const responseJson = await response.json();
+    console.log({ responseJson });
+    setNoteData(responseJson);
+  }
 
   function handleSubmit() {
     console.log({ url });
@@ -19,12 +28,26 @@ export default function Home() {
     });
   }
   return (
-    <div className={styles.container}>
+    <div className={styles.container} className="col">
       <input
         onChange={(e) => setUrl(e.target.value)}
         placeholder="iCloud link"
       ></input>
       <button onClick={handleSubmit}>Submit</button>
+      <button onClick={handleCheckStatus}>Check Status</button>
+      {noteData && noteData.ipfsHash && (
+        <a target={"_blank"} href={`https://ipfs.io/ipfs/${noteData.ipfsHash}`}>
+          view on IPFS
+        </a>
+      )}
+      <style jsx>
+        {`
+          .col {
+            flex: 1;
+            flex-direction: column;
+          }
+        `}
+      </style>
     </div>
   );
 }
