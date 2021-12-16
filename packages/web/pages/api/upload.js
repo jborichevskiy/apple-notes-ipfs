eb54d96cd433348ecf0cfe8feaf9efbb928f31c0
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-12-16 00:38:36 -0600

    Update pandoc params

--- packages/web/pages/api/upload.js
+++ packages/web/pages/api/upload.js
@@ -18,8 +18,8 @@ export default async function handler(req, res) {
     if (file) {
       const absPath = `${dataDir}/${file}`;
 
-      const args = ["-f", "rtf", "-t", "markdown"];
-      let convertedNote = nodePandoc(absPath, args, (err, result) => {
+      const args = ["-f", "rtf", "-t", "gfm"];
+      nodePandoc(absPath, args, (err, result) => {
         if (err) {
           return res.json({ error: err });
         }
