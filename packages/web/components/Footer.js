daa6c08c5aac47a5dd3aaf42cd873796ae3c1c10
Arthur Tyukayev <github_arthur@tyukayev.com>
2021-12-25 00:16:24 -0600

    Updated swr config with longer error polling timeout
    Added styleistic unicodes characters

--- packages/web/components/Footer.js
+++ packages/web/components/Footer.js
@@ -5,8 +5,8 @@ import styles from "@components/Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <Link prefetch={false} href="https://notes.site">
-        <a>made by notes.site</a>
+      <Link prefetch={false} href="https://notes.site" passHref>
+        made by notes.site
       </Link>
     </div>
   );
