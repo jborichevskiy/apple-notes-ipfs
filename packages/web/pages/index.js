11d1a6e9e9366aac20ff98e3482a2c713afe7ba1
Jon Borichevskiy <jborichevskiy@gmail.com>
2022-01-13 00:53:47 -0600

    Update styling

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -8,21 +8,39 @@ export default function Me() {
           <span className="heading">notes.site | </span>
           <span className="secondary">coming soon</span>
         </div>
-        <div>
-          <div className="row center footer">
+        <div className="footer"></div>
+        <footer id="footer">
+          <div className="content-wrap center">
             <a href="https://github.com/jborichevskiy/apple-notes-ipfs">
               github
             </a>
             <span className="seperator">·</span>
             <a href="https://t.me/+sXjvydphLF5mZGZj">telegram</a>
           </div>
-        </div>
+        </footer>
         <style>{`
+        #page-container {
+          position: relative;
+          min-height: 100vh;
+        }
+        
+        #content-wrap {
+          padding-bottom: 2rem;    /* Footer height */
+        }
+        
+        #footer {
+          position: absolute;
+          bottom: 0;
+          width: 100%;
+          height: 2rem;            /* Footer height */
+        }
+        
         .seperator {
           margin: 0 0.5rem;
         }
         .footer {
-          height: 5vh;
+          position: absolute;
+          bottom: 
         }
         .center {
           display: flex;
@@ -31,7 +49,8 @@ export default function Me() {
         }
         .middle {
           align-items: center;
-          height: 95vh;
+          height: 100%;
+          padding-bottom: 5rem;
         }
         .row {
           display: flex;
